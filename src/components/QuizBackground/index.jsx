35d8fa5d66bb797186966156ee3ee8c64072c4be
FihCapua <fihcapua@gmail.com>
2021-02-01 22:32:30 -0300

    feature: background height correction

--- src/components/QuizBackground/index.jsx
+++ src/components/QuizBackground/index.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const QuizBackground = styled.div`
   background: ${({ theme }) => theme.colors.mainBg} url('https://wallpaperaccess.com/full/531967.jpg');   
     width: 100%;
-    height: 100vh;
+    height: 145vh;
     background-size: cover;
     background-position: center center
 `;
